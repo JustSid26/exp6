6f3f438ac3a1cbad141738ef11567d42277f47aa
justSid26 <lamasidharth25@gmail.com>
2026-02-24 08:51:39 +0530

    changed msg

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "<h1>This is Siddharth Lama</h1>"
+    return "<h1>This is Siddharth Lama, COMP SCI</h1>"
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8081, debug= True)
